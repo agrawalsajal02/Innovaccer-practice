72f7dc2e6029fcc869525839994f591649d3b988
SAJAL AGRAWAL <34278780+agrawalsajal02@users.noreply.github.com>
2020-05-01 13:43:47 +0530

    as

--- src/components/RightComponent/Meet.js
+++ src/components/RightComponent/Meet.js
@@ -3,9 +3,8 @@ import React from 'react'
 function Meet() {
     return (
         <div style={{"background":"lightgrey","height":"480px"}}>
-    
-            <h1>this is Meet</h1>
-        </div>
+        <h1 style={{"margin":"auto"}}>this is Meet</h1>
+    </div>
     )
 }
 
--- src/components/RightComponent/Referrals.js
+++ src/components/RightComponent/Referrals.js
@@ -3,9 +3,8 @@ import React from 'react'
 function Referrals() {
     return (
         <div style={{"background":"lightgrey","height":"480px"}}>
-    
-            <h1>this is Referrals</h1>
-        </div>
+        <h1 style={{"margin":"auto"}}>this is Referrals</h1>
+    </div>
     )
 }
 
